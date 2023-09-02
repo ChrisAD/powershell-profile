ab2149d9b91dddf9237d12263fe252d958a1c5e4
Chris Dale <chris.adale@gmail.com>
2023-09-02 13:49:21 +0200

    Update Profile.ps1

--- Profile.ps1
+++ Profile.ps1
@@ -1,7 +1,4 @@
 Set-PSReadlineKeyHandler -Key Tab -Function Complete
 Function Get-History-Full {Get-Content (Get-PSReadlineOption).HistorySavePath}
 Set-Alias -name hist -value get-history-full -Option AllScope
-Set-PSReadlineKeyHandler -Key Tab -Function Complete
-Function Get-History-Full {Get-Content (Get-PSReadlineOption).HistorySavePath}
-Set-Alias -name hist -value get-history-full -Option AllScope
 Set-Alias -name head -value select-object -Option AllScope
