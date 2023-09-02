5a32084d751391106c2e0650f5885e9cb3bc0fee
Chris Dale <chris.adale@gmail.com>
2023-09-02 13:55:37 +0200

    Added git branch to prompt

--- Profile.ps1
+++ Profile.ps1
@@ -2,3 +2,12 @@ Set-PSReadlineKeyHandler -Key Tab -Function Complete
 Function Get-History-Full {Get-Content (Get-PSReadlineOption).HistorySavePath}
 Set-Alias -name hist -value get-history-full -Option AllScope
 Set-Alias -name head -value select-object -Option AllScope
+function prompt {
+    $currentDir = Get-Location
+    $gitBranch = $(git rev-parse --abbrev-ref HEAD 2>$null)
+    if ($gitBranch) {
+        "PS $PWD [$gitBranch]> "
+    } else {
+        "PS $PWD> "
+    }
+}
