7398c4997fd552e6bc4d37188db40f5b4b97fd76
Chris Dale <chris.adale@gmail.com>
2024-10-04 15:08:22 +0200

    Added vact alias

--- Profile.ps1
+++ Profile.ps1
@@ -3,6 +3,7 @@ Function Get-History-Full {Get-Content (Get-PSReadlineOption).HistorySavePath}
 Set-Alias -name hist -value get-history-full -Option AllScope
 Set-Alias -name head -value select-object -Option AllScope
 Set-Alias -name so -value select-object -Option AllScope
+Set-Alias -name vact -value .\venv\Scripts\activate -Option AllScope
 function prompt {
     $currentDir = Get-Location
     $gitBranch = $(git rev-parse --abbrev-ref HEAD 2>$null)
