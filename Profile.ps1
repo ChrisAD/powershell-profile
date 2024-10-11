9a8ef60b26350bc821031ff22b90f9cff6f3fe12
Chris Dale <chris.adale@gmail.com>
2024-10-11 21:38:20 +0200

    added grep

--- Profile.ps1
+++ Profile.ps1
@@ -4,6 +4,7 @@ Set-Alias -name hist -value get-history-full -Option AllScope
 Set-Alias -name head -value select-object -Option AllScope
 Set-Alias -name so -value select-object -Option AllScope
 Set-Alias -name vact -value .\venv\Scripts\activate -Option AllScope
+
 function prompt {
     $currentDir = Get-Location
     $gitBranch = $(git rev-parse --abbrev-ref HEAD 2>$null)
@@ -12,4 +13,18 @@ function prompt {
     } else {
         "PS $PWD> "
     }
+}
+
+# Instead of using SLS which if the target is a file, while open the file and read it, we use this version of sls which is similar to grep
+function grep {
+    param (
+        [Parameter(ValueFromPipeline)]
+        [string]$inputObject, # Input from the pipeline
+
+		[Parameter(Position = 0, Mandatory = $true)]
+        [string]$userInput  # Input directly from the user
+    )
+	Process {
+		$inputObject | sls -Pattern $userInput.toString()
+	}
 }
